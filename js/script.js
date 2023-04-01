f315a2d765e643db993486c0d86566929bf99361
Vlad Tipukhov <vlad1kk728@ukr.net>
2023-04-01 16:09:35 +0300

    Issue with counter

--- js/script.js
+++ js/script.js
@@ -6,32 +6,50 @@ let counter = 0;
 
 function render(){
     nmb.innerHTML = counter;
-    counter += 1
+    counter += 1;
 }
+
+function clrInt(){
+    counter = 0;
+}
+
+
+document.addEventListener('keyup', event => {
+    if (event.code === 'Space') {
+        cactus.classList.add('active');
+        let setInt = setInterval(function(){
+            render();
+        },10)
+    } 
+  })
 
 document.addEventListener("keydown", function(event){
     jump();
+    
 });
+
+
 
 function jump(){
     if(dino.classList != "jump"){
         dino.classList.add("jump");
-
-
     }
     setTimeout(function() {
         dino.classList.remove("jump")
     }, 300)
 }
+
 let isAlive = setInterval (function() {
     let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"));
     let cactusLeft = parseInt(window.getComputedStyle(cactus).getPropertyValue("left"));
 
     if(cactusLeft < 50 && cactusLeft > 0 && dinoTop >= 140) {
-        alert ("GAME OVER! SCORE: " + counter);
-        counter = 0;
-    } else {
-        render();
-    }
-    
+        let result = confirm("GAME OVER! SCORE: " + counter);
+        cactus.classList.remove('active');
+        if (result == true){
+            clrInt();
+            clearInterval(setInt);
+        }  
+    } 
 },10)
+
